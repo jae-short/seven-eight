e3e8ab8f8795971aed020608ee8d03e3c1709f5a
jae-short <kjo9658@naver.com>
2022-09-29 16:44:54 +0900

    Update process1.c

--- process1.c
+++ process1.c
@@ -1,5 +1,24 @@
+#include <stdio.h>
+
+#define TRIGGER_PIN 4
+#define ECHO_PIN 5
+#define MAX_DISTANCE 100
+
+stdio sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void loop(){
+    int distance = sonar.ping_cm();
+    delay(100)
+}
+
 int output, input;
 
 void setup(){
     pinmode(3, output);
+}
+void loop(){
+    analogwrite(3, 255);
+    delay(500);
+    analogwrite(3, 0);
+    delay(500);
 }
